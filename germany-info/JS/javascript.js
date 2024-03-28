8c491851beddeac1e5b974074058d0bb0f42265a
Zachariah Hull <83626022+RINZLER35@users.noreply.github.com>
2024-03-29 11:23:27 +1300

    Update

---
+++ germany-info/JS/javascript.js
@@ -0,0 +1,59 @@
+// // ------------------------ Header ------------------------
+// var header = '<!------------ NAV BAR ------------>\
+//     <nav class="navbar border border-dark border-2 nav-color">\
+//         <div class="container-fluid">\
+//             <a class="navbar-brand" href="index.html">Germany Guide</a>\
+//             <!--NAV BAR LINKS-->\
+//             <div class="navbar-expand-md" id="navbarSupportedContent">\
+//                 <ul class="navbar-nav me-auto mb-0 mb-lg-0">\
+//                     <li class="nav-item d-none d-md-block">\
+//                         <a class="nav-link home-link" aria-current="page" href="index.html">Home</a>\
+//                     </li>\
+//                     <li class="nav-item d-none d-md-block">\
+//                         <a class="nav-link before-leaving-link" aria-current="page" href="before-leaving.html">Before Leaving</a>\
+//                     </li>\
+//                 </ul>    \
+//             </div>\
+//             <!--NAV BAR CURTAIN MENU-->\
+//             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">\
+//                 <span class="navbar-toggler-icon"></span>\
+//             </button>\
+//             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">\
+//                 <div class="offcanvas-header nav-color">\
+//                     <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Germany Guide</h5>\
+//                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>\
+//                 </div>\
+//                 <div class="offcanvas-body">\
+//                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">\
+//                         <li class="nav-item">\
+//                             <a class="nav-link home-link" aria-current="page" href="index.html">Home</a>\
+//                         </li>\
+//                         <li class="nav-item">\
+//                             <a class="nav-link before-leaving-link" aria-current="page" href="before-leaving.html">Before Leaving</a>\
+//                         </li>\
+//                     </ul>\
+//                 </div>\
+//             </div>\
+//         </div>\
+//     </nav>'
+
+// function setHeader() {
+//     $("header").prepend(header);
+// }
+
+// setHeader();
+
+// this.page = $("header").attr('id');
+
+// function setActivePage() {
+//     console.log(page);
+//     if (page == "home-header") {
+//         $(".home-link").addClass("active");
+//     } else if (page == "before-leaving-header") {
+//         $(".before-leaving-link").addClass("active");
+//     }
+// }
+
+// setActivePage();
+
+// ------------------------ Footer ------------------------
